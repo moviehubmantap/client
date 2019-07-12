79c52d23189b0f529336340e51dad9a55eb2b9bd
imam madani <iafgon@yahoo.com>
2019-07-12 11:46:18 +0700

    googlesignin

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        url: 'http://localhost:3000/api/users/signin/google',
+        method: 'POST',
+        data:{id_token},
+
+    })
+    .done(function(data){
+        alert(data)
+        localStorage.setItem('token', data)
+    })
+    .fail(function(err) {
+        console.log(err)
+    })
+}
